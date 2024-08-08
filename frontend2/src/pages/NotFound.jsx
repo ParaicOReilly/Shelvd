8cca93cd15143d96343e4f56e9d82bc276b22e82
paraicoreilly <poreill1@tcd.ie>
2024-08-09 00:37:47 +0100

    Have a basic app structure working. Backend set up in Django. Front end in react. JWT tokens used to handle authentication. Need to add logouts and fix page navigation slightly. Home page is bare and Logout and register are functional but also quite bare. Next update needs to focus on setting up the homepage and adding logout.

---
+++ frontend2/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you are re looking for does not exist!</p>
+    </div>
+
+
+}
+
+export default NotFound
